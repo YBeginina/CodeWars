0b49f48264ff851f04cfbb11ccc26842913a5f21
YBeginina <wraith16@yandex.ru>
2022-01-31 19:09:25 +0300

    sheeps and wolf

---
+++ sheeps.js
@@ -0,0 +1,15 @@
+function warnTheSheep(queue) {
+if(queue.pop()=='wolf'){
+    return 'Pls go away and stop eating my sheep'
+}
+else {
+    let N = 1;
+    while (queue[queue.length-1]=='sheep'){
+        queue.pop();
+        N++;
+    }
+    return 'Oi! Sheep number ' + N + ' ! You are about to be eaten by a wolf!';
+}
+}
+
+console.log(warnTheSheep(['sheep', 'sheep', 'sheep', 'sheep', 'sheep', 'sheep', 'sheep', 'wolf']));
