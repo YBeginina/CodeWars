d2811df53375b751cc2ef7bb0238cb4ab74f06a6
YBeginina <wraith16@yandex.ru>
2022-02-06 15:25:41 +0300

    +

--- ArrayPlusArray.js
+++ ArrayPlusArray.js
@@ -1,8 +1,19 @@
-function arrayPlusArray(arr1, arr2) {
+function arrayPlusArray1(arr1, arr2) {
     for (let i = 0; i < arr2.length; i++) {
         arr1.push(arr2[i]);
     }
     return arr1;
   }
 
-  console.log(arrayPlusArray([1,5,9],[6,8,7]));
+  function arrayPlusArray(arr1, arr2) {
+    return sumElementsOfArr(arr1)+sumElementsOfArr(arr2);
+  }
+
+  function sumElementsOfArr(arr){
+      let sum = 0;
+      for (let i = 0; i < arr.length; i++) {
+          sum +=arr[i];
+      }
+      return sum;
+  }
+  console.log(arrayPlusArray([1,2,3],[4,5,6]));
