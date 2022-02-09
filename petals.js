0a065aeb6bc2f063bec959e01cae542af5083bc4
YBeginina <wraith16@yandex.ru>
2022-02-09 19:16:56 +0300

    petals

---
+++ petals.js
@@ -0,0 +1,6 @@
+function howMuchILoveYou(nbPetals) {
+    let phrases = ["I love you", "a little", "a lot", "passionately", "madly", "not at all"];
+    return phrases[(nbPetals-1) % phrases.length];
+}
+
+console.log(howMuchILoveYou(7));
