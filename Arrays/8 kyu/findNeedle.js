3c6546a326e06bb9270a22410a8913d0d8843241
YBeginina <wraith16@yandex.ru>
2022-02-23 23:23:10 +0300

    findNeedle

---
+++ Arrays/8 kyu/findNeedle.js
@@ -0,0 +1,7 @@
+function findNeedle(haystack) {
+    let index = haystack.findIndex((item) => item =='needle');
+    return `found the needle at position ${index}`
+  }
+
+
+console.log(findNeedle(['3', '123124234', undefined, 'needle', 'world', 'hay', 2, '3', true, false]));
