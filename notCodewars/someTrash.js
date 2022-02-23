d9e0ec5d90bf38e43787e760fd1f198d19ceaa6d
YBeginina <wraith16@yandex.ru>
2022-02-23 22:41:02 +0300

    reduce

--- notCodewars/someTrash.js
+++ notCodewars/someTrash.js
@@ -1,4 +1,9 @@
 'use strict';
 
-console.log(typeof NaN);
-console.log(2**3**2);
+//console.log(typeof NaN);
+
+
+const data = [[1,2,3],[3,4,5],[5,6,7]];
+
+const flat = data.reduce((p, i) => p.concat(i), []);
+console.log(flat);
