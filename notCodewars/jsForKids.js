ac7c3ea8e433158f163ec636e8d6334b80ddac10
YBeginina <wraith16@yandex.ru>
2022-03-16 19:47:12 +0300

    h4ck3r sp34k

--- notCodewars/jsForKids.js
+++ notCodewars/jsForKids.js
@@ -1,4 +1,4 @@
-
+/*
 let alphabet = "абвгдеёжзийклмнопрстуфхцчшщъыьэюя";
 
 let randomString = "";
@@ -10,5 +10,55 @@ while(i < 3) {
     i++;
 }
 
-console.log(randomString);
+console.log(randomString); */
 
+
+let input = "javascript is awesome";
+let output = "";
+
+/* for (let i = 0; i < input.length; i++) {
+    if(input[i]==='a'){
+        output += '4';
+    }
+    else if (input[i]==='e'){
+        output += '3';
+    }
+    else if (input[i]==='i'){
+        output += '1';
+    }
+    else if (input[i]==='o'){
+        output += '0';
+    }
+    else {
+        output += input[i];
+    }
+} */
+
+for (let i = 0; i < input.length; i++) {
+switch(input[i]) {
+    case 'a':
+    case 'A':
+        output += '4';
+        break;
+
+    case 'e':
+    case 'E':
+        output += '3';
+        break;
+
+    case 'i':
+    case 'I':
+        output += '1';
+        break;
+
+    case 'o':
+    case 'O':
+        output += '0';
+        break;
+
+    default:
+        output += input[i];
+        break;
+  }
+}
+console.log(output);
